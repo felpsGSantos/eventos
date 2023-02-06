da612ba8e57cc5e94074769dac5de2350b8e86dc
felpsGSantos <felipe.gomes04@hotmail.com.br>
2023-02-06 09:52:30 -0300

    caixa de eventos com js

---
+++ ativ.js
@@ -0,0 +1,15 @@
+function clickmouse(){
+    
+    document.querySelector('.box').innerText = "Clicou no texto"
+}
+
+function pasmouse(){
+    
+    document.querySelector('.box').innerText = "passou o mouse"
+}
+
+function tirmouse(){
+    
+    document.querySelector('.box').innerText = "Tirou o mouse"
+}
+
---
+++ script.js
@@ -0,0 +1,21 @@
+//onclick
+function clickMe(){
+    console.log("Fui cliado!")
+}
+
+function focusMe(){
+    console.log("Fui focado!")
+}
+//onblur
+function blurMe(){
+    console.log("Fui desfocado!")
+
+}
+//onchange
+function changeMe(){
+    console.log("Fui alterado!")
+}
+//onmouseover
+function overMe(){
+    document.querySelector('p').innerText="Caminhão Pipa"
+}
