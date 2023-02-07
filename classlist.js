912c319d8c7179d4bc60ccf6fbf621c270e055d0
felpsGSantos <felipe.gomes04@hotmail.com.br>
2023-02-07 11:30:46 -0300

    criando elementos

---
+++ classlist.js
@@ -0,0 +1,8 @@
+function testcla(){
+document.querySelector("#minhaDiv").classList.add("classe1","classe2","classe3")
+}
+
+function remv(){
+
+    document.querySelector("#minhaDiv").classList.remove("classe1","classe2","classe3")
+}
---
+++ elementos.js
@@ -0,0 +1,6 @@
+function botão(){
+const paragrafo = document.createElement('p')
+paragrafo.innerText = "Isso é um paragrafo"
+
+document.body.appendChild(paragrafo)
+}
