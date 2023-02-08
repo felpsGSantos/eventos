24dc0359004a2914b6b1aeb9fc4e21bc454a9691
felpsGSantos <felipe.gomes04@hotmail.com.br>
2023-02-08 11:11:59 -0300

    Fundos.js

---
+++ fundos.js
@@ -0,0 +1,25 @@
+function fonA(){
+    let element = document.querySelector('#style-text')
+    element.style.color = "blue"
+    element
+
+}
+
+function fonT(){
+    let element = document.querySelector('#style-text')
+    element.style.color = "transparent"
+
+}
+
+function fonV(){
+    let element = document.querySelector('#style-text')
+    element.style.color = "red"
+    
+
+}
+
+function fontN(){
+    let element = document.querySelector('#style-text')
+    element.style.color = "black"
+
+}
